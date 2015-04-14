352d54f9a5cc966551e149e8f2af9660194fafd7
yuriybash <yuriybash@gmail.com>
2015-04-13 22:27:57 -0700

    debugging complete

--- server/spec/ServerSpec.js
+++ server/spec/ServerSpec.js
@@ -96,7 +96,6 @@ it('Should respond with messages that were previously posted', function() {
 
     expect(res._responseCode).to.equal(200);
     var messages = JSON.parse(res._data).results;
-    // console.log("messages.length according to tests: " + messages.length)
     expect(messages.length).to.be.above(0);
     expect(messages[0].username).to.equal('Jono');
     expect(messages[0].message).to.equal('Do my bidding!');
